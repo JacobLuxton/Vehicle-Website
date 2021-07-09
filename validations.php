1cc4f164a820b971c9b7359ea3d681a25c915f72
Jacob Luxton <jacobluxton@gmail.com>
2021-07-09 14:31:55 -0400

    Add registration form and validation form to website with updated data

---
+++ validations.php
@@ -0,0 +1,48 @@
+<?php
+
+    function validate_registration($user, $conn) {
+        $errors = [];
+
+        //if email is empty return message
+        if(empty(trim($user['email']))) {
+            $errors['email'] = "Email cannot be blank";
+        }
+
+        //create an email regex
+        $email_regex ="/.+\@.+\..+/";
+
+        //if email does not match regex return message
+        if(!preg_match($email_regex, $user['email'])) {
+            $errors['email'] = "Username must be a valid email address";
+        }
+
+        //if passwords do not match notify user
+        if(!(trim($user['new-password']) == trim($user['confirm-password']))) {
+            $errors['password'] = "passwords do not match";
+            $errors['confirm'] = "passwords do not match";
+        }
+        //if password is empty return messsage
+        if(empty(trim($user['new-password']))) {
+            $errors['password'] = "password cannot be blank";
+        }
+
+        //if confirmation password is empty return message
+        if(empty(trim($user['confirm-password']))) {
+            $errors['confirm'] = "password cannot be blank";
+        }
+
+        //see if username is already taken
+        $sql = "SELECT * FROM web_users WHERE username='" . $user['email'] . "'";
+        $cmd = $conn -> prepare($sql);
+        $cmd -> execute();
+        $found_username = $cmd -> fetch();
+
+        //if username alredy taken notify user
+        if($found_username) {
+            $errors['email'] = 'Username already taken';
+        }
+
+        return $errors;
+    }
+
+?>
